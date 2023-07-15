ef40e0000d4d6266915f590b7d7908bbe447a31a
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-07-15 23:48:01 +0200

    Added decorators to help validate DTO

--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -1,5 +1,15 @@
+import { IsEmail, IsNumber, IsString } from "@nestjs/class-validator";
+import { Max, Min } from "class-validator";
+
 export class UserDTO {
+    @IsString()
     user_name: string;
+    
+    @IsNumber()
+    @Min(13)
+    @Max(130)
     user_age: number;
+    
+    @IsEmail()
     user_email: string;
 }
