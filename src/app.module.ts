43e3b7d0fce1b935039351a6507e6da4023dba10
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-07-16 18:43:23 +0200

    Added providers and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { PrismaService } from './prisma/prisma.service';
+import { PostsController } from './posts/posts.controller';
+import { PostsService } from './posts/posts.service';
 
 @Module({
   imports: [],
-  controllers: [UserController],
-  providers: [UserService, PrismaService],
+  controllers: [UserController, PostsController],
+  providers: [UserService, PrismaService, PostsService],
 })
 export class AppModule {}
